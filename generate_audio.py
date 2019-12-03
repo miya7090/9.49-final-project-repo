0a9d690e4877354feb0fb87cb1f2e9b1ac472494
M Yakubek <myakubek@outlook.com>
2019-12-02 19:56:08 -0500

    init

---
+++ generate_audio.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.io import wavfile
+
+defaultAudioFolder = "Audio files/"
+
+defaultAudioSources = ["Power A.wav", "My I A.wav", "Comeback A.wav", "Speechless A.wav", "Real Thing A.wav"]
+
+def mix_signals(signals, noiseFactor):
+    ''' Mix the signals '''
+    numSignals = signals.shape[1]
+    avg = 1/numSignals # don't exceed maximum volume
+    A = np.random.uniform(low=0, high=avg, size=(numSignals,numSignals)) # the mixing array
+    print("mixing array:", A)
+    X = np.dot(signals, A.T) # observed signal
+    X += noiseFactor * np.random.normal(size=X.shape) # add noise
+    return X
+
+def getAudioInfo(n_secs, audioFolder = defaultAudioFolder, audioSources = defaultAudioSources):
+    framerate, _ = wavfile.read(audioFolder+audioSources[0]) # get frame rate
+    n_samples = int(n_secs*framerate) # number of samples for 15 seconds of audio
+    return framerate, n_samples
+
+def load_audio_signals(n_secs, audioFolder = defaultAudioFolder, audioSources = defaultAudioSources, noiseFactor = 0, mixSignals = True):
+    # set mixSignals to False if using real-world (already mixed) recordings
+    framerate, n_samples = getAudioInfo(n_secs)
+
+    ''' Begin loading the signals '''
+    audioData = []
+    for filename in audioSources: # iterate through each audio file
+        fs, data = wavfile.read(audioFolder+filename) # load audio data
+        if fs != framerate:
+            raise ValueError # all framerates must match
+        audioData.append(data[:n_samples, 0]) # trim to 15 seconds and keep only one channel
+
+    print("Loading complete")
+    audioData = np.array(audioData, dtype=np.float).T # reformatting
+
+    ''' Mix the signals '''
+    if mixSignals:
+        X = mix_signals(audioData, noiseFactor)
+    else:
+        X = audioData
+    # export the observed signals
+    export = X.T.astype(np.int16)
+    for observed in range(len(export)):
+        wavfile.write('Audio files/source_observed_'+str(observed)+'.wav', fs, export[observed])
+    
+    ''' Pre-process the received signal '''
+    print("Pre-processing signals of shape", X.shape)
+    X_max = np.max(X) # will map to this max later
+    X = np.subtract(X, X.mean(axis=0)) # de-mean
+    X = np.divide(X, X.std(axis=0)) # de-std
+
+    return X_max, X, audioData
+
---
+++ running ica.py
@@ -0,0 +1,52 @@
+import numpy as np
+from scipy.io import wavfile
+from sklearn.decomposition import FastICA, PCA
+import matplotlib.pyplot as plt
+
+import generate_audio as audio_gen
+
+np.random.seed(100)
+n_secs = 15
+X_max, X, trueAudio = audio_gen.load_audio_signals(n_secs, mixSignals=True) # load the received signals
+numSources = X.shape[1]
+
+# load other variables
+fs, n_samples = audio_gen.getAudioInfo(n_secs)
+time = np.linspace(0, n_secs, n_samples) # time vector
+
+''' ICA '''
+ica = FastICA(n_components=numSources)
+S_ = ica.fit_transform(X)  # Reconstruct signals
+A_ = ica.mixing_  # Get estimated mixing matrix
+
+''' Post-process the estimated signal '''
+S_ -= np.min(S_) # bound left edge at zero
+S_ *= (X_max / np.max(S_))*2 # bound right edge at max*2
+S_ -= X_max # shift downwards
+
+# export the estimated signals
+export = S_.T.astype(np.int16)
+for observed in range(len(export)):
+    wavfile.write('Audio files/source_estimated_'+str(observed)+'.wav', fs, export[observed])
+
+# For comparison, compute PCA
+pca = PCA(n_components=numSources)
+H = pca.fit_transform(X)  # Reconstruct signals based on orthogonal components
+
+# #############################################################################
+# Plot results
+plt.figure()
+models = [X, trueAudio, S_, H]
+names = ['Observations (mixed signal)',
+         'True Sources',
+         'ICA recovered signals',
+         'PCA recovered signals']
+colors = ['red', 'steelblue', 'orange']
+for ii, (model, name) in enumerate(zip(models, names), 1):
+    plt.subplot(4, 1, ii)
+    plt.title(name)
+    for sig, color in zip(model.T, colors):
+        plt.plot(sig, color=color)
+
+plt.tight_layout()
+plt.show()
